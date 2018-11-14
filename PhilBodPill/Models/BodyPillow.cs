74b9ee0fd3a82fc7db5ecad4717e51dceae05fbd
inkh <jhwe92@gmail.com>
2018-11-14 10:51:38 -0800

    added BodyPillow model

---
+++ PhilBodPill/Models/BodyPillow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhilBodPill.Models
+{
+    public class BodyPillow
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
