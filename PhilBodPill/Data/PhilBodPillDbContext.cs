fb1325c538e8def273dc40c01e01ce19f787cf67
inkh <jhwe92@gmail.com>
2018-11-14 11:28:57 -0800

    completed ProductService

--- PhilBodPill/Data/PhilBodPillDbContext.cs
+++ PhilBodPill/Data/PhilBodPillDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PhilBodPill.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,7 @@ namespace PhilBodPill.Data
         {
 
         }
+
+        public DbSet<Product> Product { get; set; }
     }
 }
