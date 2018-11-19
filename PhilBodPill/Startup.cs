f2fde39c9967da6128e9fa5718d57154b26874c6
inkh <jhwe92@gmail.com>
2018-11-19 13:19:53 -0800

    deployed identity db, changed connection string

--- PhilBodPill/Startup.cs
+++ PhilBodPill/Startup.cs
@@ -44,7 +44,7 @@ namespace PhilBodPill
             });
 
             services.AddDbContext<UserDbContext>(options =>
-            options.UseSqlServer(Configuration["ConnectionStrings:IdentityConnection"])
+            options.UseSqlServer(Configuration["ConnectionStrings:IdentityProductionConnection"])
             );
 
             services.AddDbContext<PhilBodPillDbContext>(options =>
