3ccfed04ad698f38ea5533661223d347dfc1e1a6
inkh <jhwe92@gmail.com>
2018-11-20 13:14:02 -0800

    added vc

---
+++ PhilBodPill/Components/Cart.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhilBodPill.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhilBodPill.Components
+{
+    public class Cart : ViewComponent
+    {
+        private PhilBodPillDbContext _context;
+
+        public Cart(PhilBodPillDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            
+
+            return View(baskets);
+
+        }
+
+    }
+}
