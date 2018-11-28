f5f57514e8454caa041d778768b5aac5f3deb93b
Jeff Weng <jhwe92@gmail.com>
2018-11-28 00:24:51 +0000

    Revert "Merged PR 24: Checkout functionality is working.
    There is now a checkout button on the main page which takes the user to a receipt page and empties their cart.
    
    Related work items: #41, #45, #46, #47, #49, #53, #56, #61"

--- PhilBodPill/Controllers/ProductsController.cs
+++ PhilBodPill/Controllers/ProductsController.cs
@@ -13,12 +13,10 @@ namespace PhilBodPill.Controllers
     public class ProductsController : Controller
     {
         private readonly IInventory _inventory;
-        private readonly IBasket _basket;
 
-        public ProductsController(IInventory inventory, IBasket basket)
+        public ProductsController(IInventory context)
         {
-            _inventory = inventory;
-            _basket = basket;
+            _inventory = context;
         }
 
         //GET: Products
@@ -30,6 +28,7 @@ namespace PhilBodPill.Controllers
 
 
         // GET: Products/Details/5
+        [Authorize(Policy = "NoChetsAllowed")]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -145,19 +144,6 @@ namespace PhilBodPill.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             await _inventory.DeleteProduct(id);
-            return RedirectToAction(nameof(Index));
-        }
-
-        // POST: Products/CreateBasket
-        [HttpPost]
-        [Authorize]
-        public async Task<IActionResult> CreateBasket([Bind("ID,ProductID,UserID,Quantity")]Basket basket)
-        {
-            if (ModelState.IsValid)
-            {
-                await _basket.CreateBasket(basket);
-                return RedirectToAction(nameof(Index));
-            }
             return RedirectToAction(nameof(Index));
         }
 
