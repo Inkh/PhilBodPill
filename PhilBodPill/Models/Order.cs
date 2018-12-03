e4779e864e429f41b4054ee0e865f4caf89b466a
inkh <jhwe92@gmail.com>
2018-12-03 15:04:49 -0800

    added order class

---
+++ PhilBodPill/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhilBodPill.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public int userID { get; set; }
+
+        public List<Basket> Baskets { get; set; }
+    }
+}
