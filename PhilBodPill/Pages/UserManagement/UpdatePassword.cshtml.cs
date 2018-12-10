f0b537d5e1a51d3d3b7d9aee52c5a93012b90b5e
inkh <jhwe92@gmail.com>
2018-12-09 18:06:22 -0800

    user pages

---
+++ PhilBodPill/Pages/UserManagement/UpdatePassword.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PhilBodPill.Models;
+
+namespace PhilBodPill.Pages.UserManagement
+{
+    public class UpdatePasswordModel : PageModel
+    {
+        private SignInManager<User> _signInManager;
+
+        public UpdatePasswordModel(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        [BindProperty]
+        public PhilBodPill.Models.User User { get; set; }
+
+        public void OnGet(string id)
+        {
+            User = await user.
+        }
+    }
+}
